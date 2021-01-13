55abbfafef3f9c4a75cc66b4ae9ee67fb8fe9579
Agnesotti87 <77006752+Agnesotti87@users.noreply.github.com>
2021-01-13 16:39:47 +0100

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -27,11 +27,48 @@ plot(density_map, col=cl)
 #ando show points again
 points(covid_planar)
 
+#let's add also countries
+#download shapefile of coastlines and put them in directory we are using
 #install rgdal package: what is that?
 # gdal library for R, developed by OSGeo -> Open Software Geo
 install.packages("rgdal")
 library(rgdal)
+#let's use readOGR function, wich reads vector into spatial object and give it a name   
+#readOGR("filename.extension")
+coastlines <- readOGR("ne_10m_coastline.shp")
+#and plot it
+plot(density_map, col=cl)
+points(covid_planar)
+plot(coastlines, add=TRUE) # add= TRUE allows to put it on top of layers
+#change shape of points to see them better
+points(covid_planar, pch=19)
+#to export map you need function png("name export. estensione") + commands you use to layout maps + dev.off()
+png("Figure1.png")
+plot(density_map, col=cl)
+points(covid_planar)
+plot(coastlines, add=TRUE) 
+dev.off()
+# you will find the map in the wd
+#or you can create a pdf in the same way
+pdf("Figure1.pdf")
+plot(density_map, col=cl)
+points(covid_planar)
+plot(coastlines, add=TRUE) 
+dev.off()
 
+#INTERPOLATION
+#explain to R what interpolate with function is marks(object obtained from ppp) <- column you want to use
+marks(covid_planar) <- cases
+#interpolate with function: Smooth
+cases_map <- Smooth(covid_planar)
+# if appears Warning message: Least Squares Cross-Validation....don't worry: R is telling you that you have a few data for interpolation
+#now plot it and add to layout points and world map
+plot(cases,col=cl)
+points(covid_planar)
+plot(coastlines, add=TRUE)
+
+# TIPS: if it takes time to elaborate map is because of coastlines...let's simplify it!
+gSimplify(coastlines,tol=3;topologypreserve=T) #ERROREEEEEEEEEEEEEEEEE SCRIVI A DUCCIO!!!!!!!!!!
 
 
 
