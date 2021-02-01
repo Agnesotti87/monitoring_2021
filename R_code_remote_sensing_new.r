53a2a0a4a98154ed09da2aa4cc706fc7f4737498
Agnesotti87 <77006752+Agnesotti87@users.noreply.github.com>
2021-02-01 17:54:08 +0100

    Update R_code_remote_sensing_new.r

--- R_code_remote_sensing_new.r
+++ R_code_remote_sensing_new.r
@@ -6,6 +6,7 @@ library(RStoolbox)
 setwd("C:/lab/")
 #upload images and give file a name
 p224r63_2011<-brick("p224r63_2011_masked.grd")
+# brick uploads multilayer raster as multi/iperspetral satellite images
 #check metadata
 p224r63_2011
 #plot graph to see single band
@@ -18,3 +19,18 @@ par(mfrow=c(2,2))
 #assegnare una gamma di colori e visualizzare il plot
 clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
 plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
+plot(p224r63_2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(p224r63_2011$B4_sre, col=clnir)
+
+#visualizzazione RGB
+dev.off()
+plotRGB(p224r63_2011, r=3, g=2, b=1,stretch="lin")
+#visualizzazione diversa dall'occhio umano (per forza utilizzando plotRGB perchè gli schermi hanno solo questa possibilità di colore)
+plotRGB(p224r63_2011, r=4, g=3, b=2,stretch="lin")
